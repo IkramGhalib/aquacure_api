aa8ab5da77887259d9aa5dc330b901a73442176c
IkramGhalib <ikramghalib@gmail.com>
2022-10-24 10:10:27 +0500

    sws api

--- sws_api/allDevice.php
+++ sws_api/allDevice.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'opendb.php';
-$device_id=$_POST['device_id'];
-$query = "SELECT eid,AVG(temp) as temp,AVG(humidity) as hum,AVG(air_pressure) as ap,AVG(wind_speed) as ws,AVG(D_10) as d10,AVG(D2_5) as d2_5,AVG(NO) as no,AVG(No_x) as nox,AVG(NO2) as no_2,AVG(SO_2) as so_2,datetime from devices_logs where eid='$device_id'";
+
+$query = "SELECT eid,AVG(temp) as temp,AVG(humidity) as hum,AVG(air_pressure) as ap,AVG(wind_speed) as ws,AVG(D_10) as d10,AVG(D2_5) as d2_5,AVG(NO) as no,AVG(No_x) as nox,AVG(NO2) as no_2,AVG(SO_2) as so_2 from devices_logs";
 //   $query="select trid,name,longitude,latitude from transformer where trid='$device_id'";
   $result=mysqli_query($conn,$query);
   $response=array();
