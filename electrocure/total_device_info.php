d2b23a123db72196381f36b6862f54ee5a6f7384
IkramGhalib <ikramghalib@gmail.com>
2022-11-15 10:32:28 +0500

    total device info current voltage

--- electrocure/total_device_info.php
+++ electrocure/total_device_info.php
@@ -18,10 +18,21 @@ if(mysqli_num_rows($resc)>0){
         $kva3 = round($row['c3'] * $row['v2']/1000 ,2);
         $totalKVA  = round(($kva1 + $kva2 + $kva3),2);
         $current=($row['c1']+$row['c2']+$row['c3']/3);
+        // current info
+        $c1=$row['c1'];
+        $c2=$row['c2'];
+        $c3=$row['c3'];
+        $current=($row['c1']+$row['c2']+$row['c3']/3);
+
+        // voltage info
+        $v1=$row['v1'];
+        $v2=$row['v2'];
+        $v3=$row['v3'];
+        $voltage=($row['v1']+$row['v2']+$row['v3']/3);
        
         $response['error']="200";
         $response['message']="Data fetch Success";
-        $data[]=array('connection'=>$connection,'owner'=>$owner,'occupant'=>$occupant,'kva1'=>$kva1,'kva2'=>$kva2,'kva3'=>$kva3,'totalKVA'=>$totalKVA); 
+        $data[]=array('connection'=>$connection,'owner'=>$owner,'occupant'=>$occupant,'kva1'=>$kva1,'kva2'=>$kva2,'kva3'=>$kva3,'totalKVA'=>$totalKVA,'c1'=>$c1,'c2'=>$c2,'c3'=>$c3,'current'=>$current,'v1'=>$v1,'v2'=>$v2,'v3'=>$v3,'avg_voltage'=>$voltage); 
 
     }
 }
